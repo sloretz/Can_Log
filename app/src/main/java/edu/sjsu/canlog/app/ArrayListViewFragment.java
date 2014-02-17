1c3f7e1a3e385acd7f3194dff6ad927f42b6f946
Shane Loretz <a10.shane@gmail.com>
2014-02-17 12:45:47 -0800

    Custom list adapter with TWO text fields

--- app/src/main/java/edu/sjsu/canlog/app/ArrayListViewFragment.java
+++ app/src/main/java/edu/sjsu/canlog/app/ArrayListViewFragment.java
@@ -7,6 +7,8 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import java.util.Iterator;
+
 import java.util.ArrayList;
 
 /**
@@ -14,6 +16,7 @@ import java.util.ArrayList;
  */
 public class ArrayListViewFragment extends Fragment{
         private static final String ARG_LIST_VALUES = "listValues";
+        public SensorListAdapter sensorListAdapter;
 
         /**
          * Returns a new instance of this fragment for the given section
@@ -35,11 +38,18 @@ public class ArrayListViewFragment extends Fragment{
 
             ArrayList<String> values = getArguments().getStringArrayList(ARG_LIST_VALUES);
 
-            String[] strValues = new String[values.size()];
-            strValues = values.toArray(strValues);
+            sensorListAdapter = new SensorListAdapter(getActivity());
 
-            ArrayAdapter<String> adapter = new ArrayAdapter<String>(getActivity() , android.R.layout.simple_list_item_1, android.R.id.text1, strValues);
-            listView.setAdapter(adapter);
+
+            Iterator<String> strIter = values.iterator();
+            while (strIter.hasNext())
+            {
+                sensorListAdapter.addSensor(strIter.next(), "0.0");
+            }
+
+            listView.setAdapter(sensorListAdapter);
+
+            sensorListAdapter.updateSensor(0, "fitfsadf");
 
             return rootView;
         }
---
+++ app/src/main/java/edu/sjsu/canlog/app/SensorListAdapter.java
@@ -0,0 +1,127 @@
+package edu.sjsu.canlog.app;
+
+import android.database.DataSetObserver;
+import android.provider.ContactsContract;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+import android.view.LayoutInflater;
+import android.content.Context;
+import android.app.Activity;
+import java.util.ArrayList;
+import java.util.Iterator;
+import android.widget.TextView;
+
+/**
+ * Created by shane on 2/17/14.
+ */
+public class SensorListAdapter implements ListAdapter {
+
+    public class SensorStruct {
+        public SensorStruct(String n, String d) {sensorName = n; sensorData = d;};
+        public String sensorName;
+        public String sensorData;
+    }
+
+    private ArrayList<DataSetObserver> observers;
+    private ArrayList<SensorStruct> sensors;
+    private Context context;
+
+    public SensorListAdapter(Context ctx)
+    {
+        context = ctx;
+        observers = new ArrayList<DataSetObserver>();
+        sensors = new ArrayList<SensorStruct>();
+    }
+
+    public int addSensor(String name, String data)
+    {
+        sensors.add(new SensorStruct(name,data));
+        return sensors.size()-1;
+    }
+
+    public void updateSensor(int idx, String data)
+    {
+        sensors.get(idx).sensorData = data;
+        notifyObservers();
+    }
+
+    @Override
+    public boolean areAllItemsEnabled() {
+        //All items in the list are selectable
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled(int position) {
+        return true;
+    }
+
+    @Override
+    public void registerDataSetObserver(DataSetObserver observer) {
+        observers.add(observer);
+    }
+
+    @Override
+    public void unregisterDataSetObserver(DataSetObserver observer) {
+        observers.remove(observer);
+    }
+
+    public void notifyObservers()
+    {
+        Iterator<DataSetObserver> obsIter = observers.iterator();
+        while (obsIter.hasNext())
+        {
+            obsIter.next().onChanged();
+        }
+    }
+
+    @Override
+    public int getCount() {
+        return sensors.size();
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return sensors.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    @Override
+    public boolean hasStableIds() {
+        return false;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        LayoutInflater inflater = (LayoutInflater)context.getSystemService(Activity.LAYOUT_INFLATER_SERVICE);
+        View rootView = inflater.inflate(R.layout.list_item, null);
+        TextView nameView = (TextView) rootView.findViewById(R.id.sensor_name);
+        TextView dataView = (TextView) rootView.findViewById(R.id.sensor_data);
+
+        SensorStruct sen = sensors.get(position);
+        nameView.setText(sen.sensorName);
+        dataView.setText(sen.sensorData);
+        return rootView;
+    }
+
+    @Override
+    public int getItemViewType(int position) {
+        return 0;
+    }
+
+    @Override
+    public int getViewTypeCount() {
+        return 1;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return sensors.isEmpty();
+    }
+}
