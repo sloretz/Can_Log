a87f095e6a2620760214dfaa10463868422bbfc2
brlarochelle <brlarochelle@gmail.com>
2014-04-27 13:49:04 -0700

    added add row

--- app/src/main/java/edu/sjsu/canlog/app/backend/SQLlogging.java
+++ app/src/main/java/edu/sjsu/canlog/app/backend/SQLlogging.java
@@ -7,7 +7,6 @@ import android.content.Context;
  * Created by Brian on 4/27/2014.
  */
 public class SQLlogging {
-
     public class DatabaseHandler extends SQLiteOpenHelper {
 
         private static final int DATABASE_VERSION = 1;
@@ -21,7 +20,7 @@ public class SQLlogging {
         @Override
         public void onCreate(SQLiteDatabase db)
         {
-            String CREATE_TABLE= "CREATE TABLE IF NOT EXISTS" + tableVIN + "(time Integer PRIMARY KEY, x03 Integer, x04 Integer, x05 Integer, x0c Integer, x0d Integer, x11 Integer, x1f Integer, x21 Integer, x2f Integer, x30 Integer, x31 Integer, x4d Integer, x5c Integer, x5e Integer);";
+            String CREATE_TABLE= "CREATE TABLE IF NOT EXISTS" + tableVIN + " (time Integer PRIMARY KEY, x03 Integer, x04 Integer, x05 Integer, x0c Integer, x0d Integer, x11 Integer, x1f Integer, x21 Integer, x2f Integer, x30 Integer, x31 Integer, x4d Integer, x5c Integer, x5e Integer);";
             db.execSQL(CREATE_TABLE);
         }
         @Override
@@ -31,7 +30,10 @@ public class SQLlogging {
             onCreate(db);
         }
 
-        public void addRow
+        public void addRow(SQLiteDatabase db, int time, int x03, int x04, int x05, int x0c, int x0d, int x11, int x1f, int x21, int x2f, int x30, int x31, int x4d, int x5c, int x5e)
+        {
+            db.execSQL("INSERT INTO " + tableVIN + " VALUES (" + time + "," + x03 +"," + x04 + "," + x05 + "," + x0c + "," + x0d + "," + x11 + "," + x1f + "," + x21 + "," +x2f + "," + x30 + "," + x31 + "," + x4d +"," + x5c + "," +x5e);
+        }
 
     }
 }
