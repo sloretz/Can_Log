d03681676040f705ee9e107f2e8eb86ad511866f
brlarochelle <brlarochelle@gmail.com>
2014-04-28 12:30:41 -0700

    split SQLlogging

---
+++ app/src/main/java/edu/sjsu/canlog/app/backend/SQLdata.java
@@ -0,0 +1,15 @@
+package edu.sjsu.canlog.app.backend;
+
+/**
+ * Created by Brian on 4/28/2014.
+ */
+public class SQLdata {
+
+    public int time;
+    public int data;
+
+    SQLdata (int t, int d){
+        time =t;
+        data=d;
+    }
+}
