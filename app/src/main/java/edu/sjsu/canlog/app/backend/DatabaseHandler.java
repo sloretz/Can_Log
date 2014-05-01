f7d157b402f4c74f1268e512357bd6f12f54bdfa
brlarochelle <brlarochelle@gmail.com>
2014-05-01 15:26:42 -0700

    added underscore to table name since vin starts with a number

--- app/src/main/java/edu/sjsu/canlog/app/backend/DatabaseHandler.java
+++ app/src/main/java/edu/sjsu/canlog/app/backend/DatabaseHandler.java
@@ -15,11 +15,11 @@ public class DatabaseHandler extends SQLiteOpenHelper {
 
     private static final int DATABASE_VERSION = 1;
     private static final String DATABASE_NAME = "CAN_LOG";
-    private String tableVIN;
+    private final String tableVIN;
 
     public DatabaseHandler(Context context, String car) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
-        tableVIN=car;
+        tableVIN="_" + car;
     }
     @Override
     public void onCreate(SQLiteDatabase db)
