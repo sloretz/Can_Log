b1903e1afab814f4243f87140d4a25cdc0bf8d97
Shane Loretz <a10.shane@gmail.com>
2014-05-03 18:02:15 -0700

    removed semicolon

--- app/src/main/java/edu/sjsu/canlog/app/backend/Backend.java
+++ app/src/main/java/edu/sjsu/canlog/app/backend/Backend.java
@@ -186,7 +186,7 @@ public class Backend extends BluetoothService{
     {
         btWriter.write(cmd + "\r\n");
         btWriter.flush();
-        Log.d("Backend", "->> " + cmd);
+        //Log.d("Backend", "->> " + cmd);
     }
 
     protected String bt_readln() throws IOException
@@ -199,7 +199,7 @@ public class Backend extends BluetoothService{
                 //Log.d("Backend", "r");
                 nextLine = btReader.readLine();
             }
-        Log.d("Backend", "<<- " + nextLine);
+        //Log.d("Backend", "<<- " + nextLine);
         return nextLine;
     }
 
